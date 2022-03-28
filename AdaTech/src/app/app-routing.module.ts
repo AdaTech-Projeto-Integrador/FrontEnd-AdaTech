2e73fdc71c8413e432896db12f59282f83b1d762
carloskelvinn13 <kelvin132009@gmail.com>
2022-03-28 18:23:57 -0300

    Atualização dos componentes

--- AdaTech/src/app/app-routing.module.ts
+++ AdaTech/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ContatoComponent } from './contato/contato.component';
+const routes: Routes = [
 
-const routes: Routes = [];
+  {path:'',redirectTo:'login',pathMatch:'full'}, 
+
+  {path:'login', component: LoginComponent},
+{path:'cadastro', component: CadastroComponent},
+{path:'contato', component: ContatoComponent,}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

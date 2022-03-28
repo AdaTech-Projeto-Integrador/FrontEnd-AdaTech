7d54f05dcbf6bfc58b99eca2ef4c594269e9d1c4
Nadyne Barbieri <nadynemayara@live.com>
2022-03-28 14:01:30 -0300

    implementação de componentes

---
+++ AdaTech/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContatoComponent } from './contato/contato.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContatoComponent,
+    MenuComponent,
+    RodapeComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

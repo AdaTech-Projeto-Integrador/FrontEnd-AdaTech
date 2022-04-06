7e70c05d8c4f5a1942bcd9a78b45bb0e5e7581b8
Igorluan95 <igorluansouzasilva@gmail.com>
2022-04-06 18:03:10 -0300

    Consumo de API para cadastro

--- AdaTech/src/app/cadastro/cadastro.component.ts
+++ AdaTech/src/app/cadastro/cadastro.component.ts
@@ -13,6 +13,7 @@ export class CadastroComponent implements OnInit {
   usuario: Usuario = new Usuario
   confirmarSenha: string
   tipoUsuario: string
+  fotoUsuario: string
 
   constructor(
 
@@ -33,23 +34,39 @@ export class CadastroComponent implements OnInit {
     this.tipoUsuario = event.target.value
   }
 
+  validarFoto(event: any) {
+    this.fotoUsuario = event.target.value
+  }
+
   cadastrar() {
-
-    if (this.tipoUsuario == "tipo") {
-      alert('Escolha um tipo de usuário!')
-    } else {
-      this.usuario.tipo = this.tipoUsuario
-    }
-
 
     if (this.usuario.senha != this.confirmarSenha) {
       alert('As senhas estão incorretas!')
-    } else {
+    } else if (this.tipoUsuario != "profissional" && this.tipoUsuario != "org") {
+      alert('Selecione o tipo de perfil!')
+    }
+
+    else {
+
+      if (this.fotoUsuario == null) {
+        this.fotoUsuario = "https://i.imgur.com/wBsUWjq.png"
+      }
+
+      this.usuario.tipo = this.tipoUsuario
+      this.usuario.foto = this.fotoUsuario
       this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
         this.usuario = resp
         this.router.navigate(['/login'])
         alert('Usuário cadastrado com sucesso!')
-      })
+      },
+
+        erro => {
+          if (erro.status == 500 || erro.status == 401) {
+            alert('Preencha os campos obrigatórios corretamente!')
+          }
+        }
+
+      )
     }
 
   }
